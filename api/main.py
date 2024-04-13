3dc2be5e39bedf73e3b06e98d507bba2481eb3b2
Padma <padmacs2000@yahoo.com>
2024-04-12 18:29:35 -0700

    added an endpoint to fetch a photo from Unsplash api

---
+++ api/main.py
@@ -0,0 +1,35 @@
+import os
+from email.charset import add_codec
+
+import requests
+from dotenv import load_dotenv
+from flask import Flask, request
+
+load_dotenv(dotenv_path="./.env.local")
+
+UNSPLASH_URL='https://api.unsplash.com/photos/random'
+UNSPLASH_KEY=os.environ.get("UNSPLASH_KEY", "")
+
+if not UNSPLASH_KEY:
+  raise EnvironmentError("Please create .env.local and add the UNSPLASH_KEY to it")
+
+app = Flask(__name__)
+
+@app.route("/new-image")
+def new_image():
+  word = request.args.get("query")
+  
+  headers = {
+    "Authorization": "Client-ID " + UNSPLASH_KEY,
+    "Accept-Version": "v1"
+  }
+  params = {
+    "query": word
+  }
+  response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
+  data = response.json()
+  return data
+        
+
+if __name__ == "__main__":
+  app.run(host="0.0.0.0", port=5050)
