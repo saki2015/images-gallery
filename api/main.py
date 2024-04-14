99479d7d78151b4211dbaae0b22c50a1d6d091c5
Padma <padmacs2000@yahoo.com>
2024-04-13 17:49:33 -0700

    linting changes in python files.

--- api/main.py
+++ api/main.py
@@ -7,31 +7,33 @@ from flask_cors import CORS
 
 load_dotenv(dotenv_path="./.env.local")
 
-UNSPLASH_URL='https://api.unsplash.com/photos/random'
-UNSPLASH_KEY=os.environ.get("UNSPLASH_KEY", "")
-DEBUG=bool(os.environ.get("DEBUG", ""))
+UNSPLASH_URL = 'https://api.unsplash.com/photos/random'
+UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY", "")
+DEBUG = bool(os.environ.get("DEBUG", ""))
 
 if not UNSPLASH_KEY:
-  raise EnvironmentError("Please create .env.local and add the UNSPLASH_KEY to it")
+    raise EnvironmentError(
+        "Please create .env.local and add the UNSPLASH_KEY to it")
 
 app = Flask(__name__)
 CORS(app)
 
+
 @app.route("/new-image")
 def new_image():
-  word = request.args.get("query")
-  
-  headers = {
-    "Authorization": "Client-ID " + UNSPLASH_KEY,
-    "Accept-Version": "v1"
-  }
-  params = {
-    "query": word
-  }
-  response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
-  data = response.json()
-  return data
-        
+    word = request.args.get("query")
+
+    headers = {
+        "Authorization": "Client-ID " + UNSPLASH_KEY,
+        "Accept-Version": "v1"
+    }
+    params = {
+        "query": word
+    }
+    response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
+    data = response.json()
+    return data
+
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=5050, debug=DEBUG)
+    app.run(host="0.0.0.0", port=5050, debug=DEBUG)
